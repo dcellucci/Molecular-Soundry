5c9e764201cb71563cfd3eb37c2e3f7f3bd0ebc9
Daniel Cellucci <visualbasicimp@gmail.com>
2011-06-25 01:32:54 -0400

    Adding ruby scripts

--- atoms/spectraparser.rb
+++ atoms/spectraparser.rb
@@ -8,47 +8,11 @@
 
 #Daniel Walton Cellucci - June 2011 - danielwcellucci.com
 
-### CODE DESIGNED TO RECURSIVELY READ DIRECTORIES AND ###
-########### CHANGE THE FILENAMES OF THE DATA ############
-#Dir.open("data")
-#Dir.glob("**/*.txt").each {|filename|
-#  newname = File.split(filename)[1]
-#  newname = File.split(filename)[0]<<"/"<<newname.split(".txt")[0]<<".dat"
-#  File.rename(filename, newname)
-#  print newname << " \n"
-#  } 
 
-output = File.open("atomic.sc", 'w')
-output.write("(")
 Dir.open("data")
-Dir.glob("**/*.dat").each {|filename|
-  f = File.open(filename, 'r')
-  spectlines = []
-  total = 0
-  f.readlines.each { |line|
-    line = line.split(" ")
-    line.map!{|i| i.to_f}
-    line[0] = 1.0/line[0]*2997924.58
-    total = total + line[1]
-    spectlines << line
-  }
-  puts spectlines.length
-  f.close()
-  temp = "///////////////////////////////////////////////////"
-  form = "#{temp}\n//#{filename}\n#{temp}\n"
-  #form = form << "SynthDef.new(\"#{File.split(filename)[1].split(".dat")[0]}\",\{"
-  #form = form << "Out.ar(0,"
-  form = form << "a = ["
-  spectlines.each{|i|
-    form = form << "[#{i[0]}, #{(i[1]/total).to_s[0...8]}],"
-    #form = form << "SinOsc.ar(#{i[0]})*#{3.0/spectlines.length}+"
-  }
-  form = form[0...-1]
-  
-  #form = form << ";)\}).send(s);\n\n"
-  form = form << "];\n\n"
-  #puts form 
-  output.write(form)
-}
-output.write(")")
-output.close()
+Dir.glob("**/*.txt").each {|filename|
+  newname = File.split(filename)[1]
+  newname = File.split(filename)[0]<<"/"<<newname.split(".txt")[0]<<".dat"
+  File.rename(filename, newname)
+  print newname << " \n"
+  } 
